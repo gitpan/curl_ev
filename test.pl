dc694f4db3a4b8bf30693c2c4fcbfa84d9243e12
Nick Kostyria <kni@cpan.org>
2013-09-29 04:06:26 +0000

    Import of KNI/curl_ev-0.01.4 from CPAN.
    gitpan-cpan-distribution: curl_ev
    gitpan-cpan-version:      0.01.4
    gitpan-cpan-path:         KNI/curl_ev-0.01.4.tar.gz
    gitpan-cpan-author:       KNI
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -3,4 +3,14 @@ use warnings;
 
 use Test::More tests => 1;
 
-BEGIN { use_ok('Net::Curl::Multi::EV') };
+
+SKIP: {
+	eval {
+		require Net::Curl::Multi;
+		Net::Curl::Multi->can('CURLMOPT_TIMERFUNCTION') or
+		die "Net::Curl::Multi is missing timer callback. Rebuild Net::Curl with libcurl 7.16.0 or newer\n";
+	};
+
+	skip "Net::Curl::Multi::EV: $@", 1 if $@;
+	require_ok('Net::Curl::Multi::EV');
+};
